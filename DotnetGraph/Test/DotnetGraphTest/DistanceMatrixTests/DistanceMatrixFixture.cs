cb677013a90a8d306700daa32be647edf160d53a
Justus Bonz <justus.bonz@uni-hamburg.de>
2019-11-29 20:41:32 +0100

    Added basic distance matrix algorithm

---
+++ DotnetGraph/Test/DotnetGraphTest/DistanceMatrixTests/DistanceMatrixFixture.cs
@@ -0,0 +1,32 @@
+﻿using DotnetGraph.Algorithms.Contracts;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace DotnetGraphTest.DistanceMatrixTests
+{
+    [TestClass]
+    public abstract class DistanceMatrixFixture
+    {
+        protected abstract IDistanceMatrixAlgorithm GetDistanceMatrixAlgorithm();
+
+        [TestMethod]
+        public void SmallGraph()
+        {
+            var arcs = DirectedGraphGenerator.GetSmallGraph();
+            var algorithm = GetDistanceMatrixAlgorithm();
+            var matrix = algorithm.GetDistanceMatrix(arcs);
+            Assert.AreEqual(0, matrix.GetDistance("A", "A"));
+            Assert.AreEqual(4, matrix.GetDistance("A", "B"));
+            Assert.AreEqual(4, matrix.GetDistance("A", "C"));
+            Assert.AreEqual(1, matrix.GetDistance("A", "D"));
+            Assert.AreEqual(2, matrix.GetDistance("A", "E"));
+            Assert.AreEqual(3, matrix.GetDistance("A", "F"));
+
+            Assert.AreEqual(4, matrix.GetDistance("B", "A"));
+            Assert.AreEqual(0, matrix.GetDistance("B", "B"));
+            Assert.AreEqual(4, matrix.GetDistance("B", "C"));
+            Assert.AreEqual(3, matrix.GetDistance("B", "D"));
+            Assert.AreEqual(2, matrix.GetDistance("B", "E"));
+            Assert.AreEqual(3, matrix.GetDistance("B", "F"));
+        }
+    }
+}
---
+++ DotnetGraph/Test/DotnetGraphTest/DistanceMatrixTests/MultipleShortestPathTest.cs
@@ -0,0 +1,14 @@
+﻿using DotnetGraph.Algorithms.Contracts;
+using DotnetGraph.Algorithms.DistanceMatrix;
+using DotnetGraph.Algorithms.ShortestPathTree;
+
+namespace DotnetGraphTest.DistanceMatrixTests
+{
+    public class MultipleShortestPathTest : DistanceMatrixFixture
+    {
+        protected override IDistanceMatrixAlgorithm GetDistanceMatrixAlgorithm()
+        {
+            return new MultipleShortestPath(new Fifo());
+        }
+    }
+}
