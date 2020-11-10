d202db82f868dcd8483653d85c94cab3fd264ec8
Matthes <mk.public@outlook.com>
2020-11-10 20:50:48 +0100

    Added DfS Algorithm and Test

---
+++ DotnetGraph/Source/DotnetGraph/Algorithms/DfSearch/DepthFirstSearch.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace DotnetGraph.Algorithms.DfSearch
+{
+    public class DepthFirstSearch
+    {
+        private int _time;
+
+        public void Run(IEnumerable<DfSearchNode> nodes)
+        {
+            if (nodes == null || !nodes.Any())
+                throw new ArgumentException($"{nameof(nodes)} is null or empty.");
+
+            Initialize(nodes);
+            foreach (DfSearchNode n in nodes)
+            {
+                if(n.SearchState == DfSearchState.Undiscovered)
+                {
+                    Visit(n);
+                }
+            }
+        }        
+
+        private void Visit(DfSearchNode node)
+        {
+            _time++;
+            node.DiscoveredAt = _time;
+            node.SearchState = DfSearchState.Discovered;
+            foreach (DfSearchArc incidentArc in node.OutgoingArcs)
+            {
+                DfSearchNode adjacentNode = incidentArc.Node2;
+                if(adjacentNode.SearchState == DfSearchState.Undiscovered)
+                {
+                    adjacentNode.PredecessorNode = node;
+                    Visit(adjacentNode);
+                }
+            }
+            node.SearchState = DfSearchState.Visited;
+            _time++;
+            node.FinishedAt = _time;
+        }
+
+        private void Initialize(IEnumerable<DfSearchNode> nodes)
+        {
+            _time = 0;
+            foreach (DfSearchNode n in nodes)
+            {
+                n.SearchState = DfSearchState.Undiscovered;
+                n.PredecessorNode = null;
+            }
+        }
+
+    }
+}
--- DotnetGraph/Source/DotnetGraph/Algorithms/DfSearch/DfSearchArc.cs
+++ DotnetGraph/Source/DotnetGraph/Algorithms/DfSearch/DfSearchArc.cs
@@ -1,10 +1,17 @@
 ﻿using DotnetGraph.Model.Properties;
-
+using System;
 
 namespace DotnetGraph.Algorithms.DfSearch
 {
     public class DfSearchArc : IConnectsNodes<DfSearchNode>
     {
+
+        public DfSearchArc(DfSearchNode n1, DfSearchNode n2)
+        {
+            Node1 = n1 ?? throw new ArgumentNullException(nameof(n1));
+            Node2 = n2 ?? throw new ArgumentNullException(nameof(n2));
+        }
+
         public DfSearchNode Node1 { get; }
 
         public DfSearchNode Node2 { get; }
